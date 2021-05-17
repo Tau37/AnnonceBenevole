e48ae479561a27749ac13ddc1731151fe04bbfb9
apprenant <Tau37@live.fr>
2021-05-17 15:14:25 +0200

    formulaire d'enregistrement

--- src/Security/Authenticator.php
+++ src/Security/Authenticator.php
@@ -95,8 +95,9 @@ class Authenticator extends AbstractFormLoginAuthenticator implements PasswordAu
             return new RedirectResponse($targetPath);
         }
 
+        return new RedirectResponse($this->urlGenerator->generate('home'));
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
     protected function getLoginUrl()
