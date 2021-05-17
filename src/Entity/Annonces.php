186e5eb27f76695ce57620ef1798567da95e2f5f
apprenant <Tau37@live.fr>
2021-05-17 14:27:42 +0200

    creation table user + annonces (sans liaison)

---
+++ src/Entity/Annonces.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AnnoncesRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=AnnoncesRepository::class)
+ */
+class Annonces
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $annonces;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $date;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $categorie;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $valide;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAnnonces(): ?string
+    {
+        return $this->annonces;
+    }
+
+    public function setAnnonces(string $annonces): self
+    {
+        $this->annonces = $annonces;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getCategorie(): ?string
+    {
+        return $this->categorie;
+    }
+
+    public function setCategorie(string $categorie): self
+    {
+        $this->categorie = $categorie;
+
+        return $this;
+    }
+
+    public function getValide(): ?bool
+    {
+        return $this->valide;
+    }
+
+    public function setValide(bool $valide): self
+    {
+        $this->valide = $valide;
+
+        return $this;
+    }
+}
