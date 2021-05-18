0af18e5cbdffe65d400f51973a2cfcee75a708a6
apprenant <Tau37@live.fr>
2021-05-18 16:10:21 +0200

    affichage des annonces sur le home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,19 +2,24 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\AnnoncesRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/", name="home")
+     * @Route("/", name="home", methods={"GET"})
      */
-    public function index(): Response
+    public function index(AnnoncesRepository $annoncesRepository): Response
     {
+        $annonces = $annoncesRepository->findAll();
+        
         return $this->render('home/index.html.twig', [
+            'annonces' => $annonces,
             'controller_name' => 'HomeController',
         ]);
     }
+
 }
