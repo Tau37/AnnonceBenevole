dbd472964fd2f5248ce68f943cf5fd3f83816fd3
Thomas <izamazzio@gmail.com>
2021-05-26 09:31:34 +0200

    tri catégorie des annonces sur la page annonce public

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -25,11 +25,18 @@ class HomeController extends AbstractController
     /**
      * @Route("/liste/annonces", name="annonces", methods={"GET"})
      */
-    public function annonce(AnnoncesRepository $annoncesRepository): Response
+    public function annonce(AnnoncesRepository $annoncesRepository, CategoriesRepository $categoriesRepository): Response
     {
 
-        
-        $annonces = $annoncesRepository->findBy(["valide" => "1" ] ,["date" => "DESC"]);
+        // je récupére le nom des catégorie en fonction de l'id de la liaison (linkCategorie)
+        if (isset($_GET["id_categorie"]) && !empty($_GET["id_categorie"])) {
+            $id_categorie = $_GET["id_categorie"];
+            $id_categorie = $categoriesRepository->findBy(["id"=> $id_categorie]);
+            $annonces = $annoncesRepository->findBy(["linkCategorie"=> $id_categorie,"valide" => "1"],["date" => "DESC"]);
+        } else {
+            $id_categorie = null;
+            $annonces = $annoncesRepository->findBy(["valide" => "1" ] ,["date" => "DESC"]);
+        }
         //$annonces = $annoncesRepository->findAll();
         return $this->render('annonces/annonces.html.twig', [
             'annonces' => $annonces,
